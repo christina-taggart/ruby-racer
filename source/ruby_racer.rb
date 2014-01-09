717f7525a6305507e5abd0a51fd000b2e8b8dba2
& <pair+leishman.nifengle@devbootcamp.com>
2014-01-09 15:36:48 -0800

    Added landmines and boosts

--- source/ruby_racer.rb
+++ source/ruby_racer.rb
@@ -8,6 +8,7 @@ class RubyRacer
     @length = length
     @die = Die.new
     @landmine = false
+    @boost = false
   end
 
   # Returns +true+ if one of the players has reached
@@ -24,16 +25,22 @@ class RubyRacer
   # Rolls the dice and advances +player+ accordingly
   def advance_player!(player)
     @current_player_name = player[0]
-    if landmine_checker(player)
-      @players[player] /= 2
-    else
-      @players[player] += @die.roll
+    landmine_checker(player)
+    boost_checker(player)
+    @players[player] += @die.roll
+  end
+
+  def boost_checker(player)
+    @boost = false
+    if rand(5) == 3
+      @boost = true
+      @players[player] += 10
     end
   end
 
   def landmine_checker(player)
     @landmine = false
-    if rand(5) == 3
+    if rand(5) == 3 && @players[player] >= 6
       @landmine = true
       @players[player] /= 2
     end
@@ -49,16 +56,26 @@ class RubyRacer
     @players.each do |player|
       name = player[0]
       name_offset = max_name_length - name.length
+
       player[1] > @length ? car_distance = @length : car_distance = player[1]
       @length - car_distance >= 0 ? offset = @length - car_distance : offset = 0
 
+      if name == @current_player_name
+        if @landmine
+          special_effect = "LANDMINE!!!"
+        elsif @boost
+          special_effect = "TURBO-BOOST!!!"
+        end
+      end
+
       puts <<-eos
-      #{" " * max_name_length}|#{"-" * @length} |
-      #{" " * name_offset}#{name}|#{"=" * car_distance}>#{" " * offset}| #{"LANDMINE!!!" if @landmine && name == @current_player_name}
-      #{" " * max_name_length}|#{"_" * @length} |
+        #{" " * max_name_length}|#{"-" * @length} |
+        #{" " * name_offset}#{name}|#{"=" * car_distance}>#{" " * offset}| #{special_effect}
+        #{" " * max_name_length}|#{"_" * @length} |
       eos
     end
   end
+
 end
 
 players = ['a', 'b', 'alex', 'patrick']
@@ -76,7 +93,6 @@ until game.finished?
     # We print the board first so we see the initial, starting board
     game.print_board
     game.advance_player!(player)
-
     # We need to sleep a little, otherwise the game will blow right past us.
     # See http://www.ruby-doc.org/core-1.9.3/Kernel.html#method-i-sleep
     sleep(0.5)
