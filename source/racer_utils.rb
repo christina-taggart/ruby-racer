368e214050da06e18c3fd95e741fce3e68a780d1
Apprentice <apprentice@dbc26.local>
2014-01-09 22:07:27 -0800

    Ruby Racer Added AND ITS AWESOME

--- source/racer_utils.rb
+++ source/racer_utils.rb
@@ -2,7 +2,7 @@ class Die
   def initialize(sides = 6)
     @sides = sides
   end
-  
+
   # Remember: rand(N) randomly returns one of N consecutive integers, starting at 0
   # So rand(N) returns a random integer in (0..N-1)
   # And 1 + rand(N) returns a random integer in (1..N)
@@ -20,7 +20,7 @@ end
 
 # Clear the screen
 def clear_screen!
-  print "\e[2J"
+  print "\e[2J" #system(clear)
 end
 
 # Moves cursor to the top left of the terminal
--- source/ruby_racer.rb
+++ source/ruby_racer.rb
@@ -1,22 +1,47 @@
 require_relative 'racer_utils'
 
+class Player
+  attr_accessor :position, :name
+  def initialize(position, name)
+    @position = position
+    @name = name
+  end
+end
+
+
+
 class RubyRacer
-  attr_reader :players, :length
+  attr_accessor :players, :length
 
   def initialize(players, length = 30)
+    @players = players
+    @length = length
+    @die = Die.new
+    @won = false
+    @victor = ""
+    @track = []
   end
+
 
   # Returns +true+ if one of the players has reached
   # the finish line, +false+ otherwise
   def finished?
+    @won
   end
 
   # Returns the winner if there is one, +nil+ otherwise
   def winner
+    @victor.name
   end
 
   # Rolls the dice and advances +player+ accordingly
   def advance_player!(player)
+    player.position += @die.roll
+    if player.position >= @length
+      player.position = @length
+      @won = true
+      @victor = player
+    end
   end
 
   # Prints the current game board
@@ -24,20 +49,32 @@ class RubyRacer
   # and you should use the "reputs" helper to print over
   # the previous board
   def print_board
+    system("clear")
+    @players.each do |player|
+      puts "#{player.name.ljust(10)} |" + "#{"=" * player.position}>".ljust(@length + 1) + "|"
+    end
+    # @track[player.position] = "|    #{player.name}"
+    # p @track.join
   end
+
 end
 
-players = ['a', 'b']
-
+#players = ['a', 'b']
+#players = Player.new(0,"upper")
+#p players
+a = Player.new(0, "Horatio")
+b = Player.new(0, "Sylvester")
+players = [a,b]
 game = RubyRacer.new(players)
 
+
 # This clears the screen, so the fun can begin
-clear_screen!
+# clear_screen!
 
 until game.finished?
   players.each do |player|
     # This moves the cursor back to the upper-left of the screen
-    move_to_home!
+    # move_to_home!
 
     # We print the board first so we see the initial, starting board
     game.print_board
@@ -45,11 +82,14 @@ until game.finished?
 
     # We need to sleep a little, otherwise the game will blow right past us.
     # See http://www.ruby-doc.org/core-1.9.3/Kernel.html#method-i-sleep
-    sleep(0.5)
+    sleep(0.2)
   end
 end
 
+#clear screen?
 # The game is over, so we need to print the "winning" board
 game.print_board
 
 puts "Player '#{game.winner}' has won!"
+
+
